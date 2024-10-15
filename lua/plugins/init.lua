baff3c3756a690ad86571b6d53c7dbe635325ec6
[Saiiru] <[sairufsa@gmail.com]>
2024-10-15 20:27:47 -0300

    feat(nvim-cmp): add snippet icons and improve completion UI

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,9 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+     'tzachar/cmp-tabnine',
+     build = './install.sh',
+     dependencies = 'hrsh7th/nvim-cmp',
+ },
 }
