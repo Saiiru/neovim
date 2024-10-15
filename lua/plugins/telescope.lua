6c857f6bec0d0de7ca69291f70d52dc79951dbd4
[Saiiru] <[sairufsa@gmail.com]>
2024-10-15 20:47:17 -0300

    feat(telescope): add basic configuration for Telescope plugin

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,75 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.1",
+  requires = { { "nvim-lua/plenary.nvim" } },
+  config = function()
+    require("telescope").setup {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case"
+        },
+        prompt_prefix = "🔍 ",
+        selection_caret = " ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            mirror = false,
+            preview_cutoff = 120,
+            prompt_position = "bottom",
+            results_width = 0.6
+          },
+          vertical = {
+            mirror = false
+          },
+          width = 0.75,
+          height = 1,
+          preview_cutoff = 120,
+          preview_width = 0.6
+        },
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        file_ignore_patterns = { "node_modules" },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+        color_devicons = true,
+        use_less = true,
+        set_env = { ["COLORTERM"] = "truecolor" },
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+        },
+        live_grep = {
+          theme = "dropdown",
+        },
+        buffers = {
+          theme = "dropdown",
+        },
+        help_tags = {
+          theme = "dropdown",
+        },
+      },
+      extensions = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
+        --   extension_config_key = value,
+        -- }
+      },
+    }
+  end,
+}
