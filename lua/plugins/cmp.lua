f37e929932ee5293235e85ded5747fc9035ec5b6
[Saiiru] <[sairufsa@gmail.com]>
2024-10-21 23:49:15 -0300

    Refactor: testing utils for better LSP organization

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,87 +1,101 @@
--- Auto-completion / Snippets
-return {
-  -- https://github.com/hrsh7th/nvim-cmp
-  'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
-  dependencies = {
-    -- Snippet engine & associated nvim-cmp source
-    -- https://github.com/L3MON4D3/LuaSnip
-    'L3MON4D3/LuaSnip',
-    -- https://github.com/saadparwaiz1/cmp_luasnip
-    'saadparwaiz1/cmp_luasnip',
-
-    -- LSP completion capabilities
-    -- https://github.com/hrsh7th/cmp-nvim-lsp
-    'hrsh7th/cmp-nvim-lsp',
-
-    -- Additional user-friendly snippets
-    -- https://github.com/rafamadriz/friendly-snippets
-    'rafamadriz/friendly-snippets',
-    -- https://github.com/hrsh7th/cmp-buffer
-    'hrsh7th/cmp-buffer',
-    -- https://github.com/hrsh7th/cmp-path
-    'hrsh7th/cmp-path',
-    -- https://github.com/hrsh7th/cmp-cmdline
-    'hrsh7th/cmp-cmdline',
+    return {
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    config = function()
+        require("tailwindcss-colorizer-cmp").setup({
+            color_square_width = 3,
+            color_square_height = 1,
+            enable_tailwind = true,
+            color_background = true,
+            color_text = true,
+            color_text_background = false,
+            color_square_integration = true,
+            auto_update = true,
+        })
+    end,
   },
-  config = function()
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
-    require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup({})
+  {
+    "b0o/schemastore.nvim",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "tzachar/cmp-tabnine", -- Add cmp-tabnine
+      "github/copilot.vim",  -- Add copilot
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+      require('luasnip.loaders.from_vscode').lazy_load()
+      luasnip.config.setup({})
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = {
-        completeopt = 'menu,menuone,noinsert',
-      },
-      mapping = cmp.mapping.preset.insert {
-        ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
-        ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- scroll backward
-        ['<C-f>'] = cmp.mapping.scroll_docs(4), -- scroll forward
-        ['<C-Space>'] = cmp.mapping.complete {}, -- show completion suggestions
-        ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
-	-- Tab through suggestions or when a snippet is active, tab to the next argument
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-	-- Tab backwards through suggestions or when a snippet is active, tab to the next argument
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- lsp 
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-      window = {
-        -- Add borders to completions popups
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-    })
-  end,
- }
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "cmp_tabnine" }, -- Add Tabnine to sources
+        }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+      })
 
+      local tabnine = require('cmp_tabnine.config')
+      tabnine:setup({
+          max_lines = 1000,
+          max_num_results = 20,
+          sort = true,
+      })
+    end,
+  },
+}
