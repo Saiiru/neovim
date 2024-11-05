4c826019b2337955f205e2fca13e6bb8a41fefd8
[Saiiru] <[sairufsa@gmail.com]>
2024-11-05 14:50:29 -0300

    feat: reorganize project structure for improved modularity
    - Restructured directories to enhance clarity and accessibility.
    - Grouped related configuration files and plugins for easier management.
    - Updated initialization scripts for seamless loading of new configurations.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
 local ac = vim.api.nvim_create_autocmd
 local ag = vim.api.nvim_create_augroup
 
@@ -5,7 +8,7 @@ local ag = vim.api.nvim_create_augroup
 ac("BufRead", {
   pattern = ".env",
   callback = function()
-    vim.diagnostic.enable(false)
+    vim.diagnostic.disable(false)
   end,
 })
 
