1570f017900d438af10686cc62bf4c1fff9d35b4
[Saiiru] <[sairufsa@gmail.com]>
2024-11-28 14:37:00 -0300

    Restarted based on tinyvim

--- init.lua
+++ init.lua
@@ -1,41 +1,25 @@
--- Declare the path where lazy will clone plugin code
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+require "options"
+require "mappings"
+require "commands"
 
--- Check to see if lazy itself has been cloned, if not clone it into the lazy.nvim directory
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+-- bootstrap plugins & lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim" -- path where its going to be installed
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
 end
--- Add the path to the lazy plugin repositories to the vim runtime path
+
 vim.opt.rtp:prepend(lazypath)
 
--- Declare a few options for lazy
-local opts = {
-	change_detection = {
-		-- Don't notify us every time a change is made to the configuration
-		notify = false,
-	},
-	checker = {
-		-- Automatically check for package updates
-		enabled = true,
-		-- Don't spam us with notification every time there is an update available
-		notify = false,
-	},
-}
+local plugins = require "plugins"
 
--- Load the options from the config/options.lua file
-require("sairu.config.options")
--- Load the keymaps from the config/keymaps.lua file
-require("sairu.config.keymaps")
--- Load the auto commands from the config/autocmds.lua file
-require("sairu.config.autocmds")
--- Setup lazy, this should always be last
--- Tell lazy that all plugin specs are found in the plugins directory
--- Pass it the options we specified above
-require("lazy").setup("sairu.plugins", opts)
+require("lazy").setup(plugins, require "lazy_config")
+
+vim.cmd "colorscheme nightfox"
