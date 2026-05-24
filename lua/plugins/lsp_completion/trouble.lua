6290559197aa0c8b221c5fc9ab8fb3616e9c9554
[Saiiru] <sairufsa@gmail.com>
2026-05-24 12:32:15 -0300

    feat(nvim): add trouble diagnostics surface

---
+++ lua/plugins/lsp_completion/trouble.lua
@@ -0,0 +1,87 @@
+return {
+  "folke/trouble.nvim",
+  event = "LspAttach",
+  cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    focus = false,
+    auto_preview = false,
+    warn_no_results = false,
+    win = {
+      type = "split",
+      position = "bottom",
+      size = 0.32,
+      border = "single",
+    },
+    modes = {
+      diagnostics = {
+        auto_open = false,
+        auto_close = false,
+        win = {
+          position = "bottom",
+          size = 0.32,
+        },
+      },
+      symbols = {
+        focus = false,
+        win = {
+          position = "right",
+          size = 0.36,
+        },
+      },
+      quickfix = {
+        focus = false,
+      },
+      loclist = {
+        focus = false,
+      },
+      lsp_references = {
+        focus = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>xx",
+      function()
+        require("trouble").toggle({ mode = "diagnostics", filter = { buf = 0 }, focus = false })
+      end,
+      desc = "Trouble buffer diagnostics",
+    },
+    {
+      "<leader>xX",
+      function()
+        require("trouble").toggle({ mode = "diagnostics", focus = false })
+      end,
+      desc = "Trouble workspace diagnostics",
+    },
+    {
+      "<leader>xs",
+      function()
+        require("trouble").toggle({ mode = "symbols", focus = false })
+      end,
+      desc = "Trouble symbols",
+    },
+    {
+      "<leader>xq",
+      function()
+        require("trouble").toggle({ mode = "quickfix", focus = false })
+      end,
+      desc = "Trouble quickfix",
+    },
+    {
+      "<leader>xl",
+      function()
+        require("trouble").toggle({ mode = "loclist", focus = false })
+      end,
+      desc = "Trouble loclist",
+    },
+    {
+      "<leader>xr",
+      function()
+        require("trouble").toggle({ mode = "lsp_references", focus = false })
+      end,
+      desc = "Trouble references",
+    },
+  },
+}
